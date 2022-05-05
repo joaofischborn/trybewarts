c9ee578bc9279277f9028056b330ecb062b6fa19
Henryck <ryck302@gmail.com>
2022-05-05 19:11:32 -0300

    requisitos 15 ao 19

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const button = document.querySelector('#login-button');
+const submitButton = document.querySelector('#submit-btn');
+const agreeInput = document.querySelector('#agreement');
 
 button.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -8,4 +10,8 @@ button.addEventListener('click', () => {
   } else {
     alert('Email ou senha inválidos.');
   }
+});
+
+agreeInput.addEventListener('click', () => {
+  submitButton.disabled = !agreeInput.checked;
 });
